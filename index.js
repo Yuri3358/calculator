58c0b8a08dfbc6618ab70e9432789918ece78974
yuri3358 <yurialmeida917@gmail.com>
2022-08-18 20:01:13 -0300

    CSS organization and new features

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function Start() {
+function start() {
     const numbers = document.getElementsByClassName('number')
     const output = document.getElementById('display')
 
@@ -11,18 +11,23 @@ function Start() {
             list_numbers.push(element.target.id)
             console.log(list_numbers)
 
-            if (list_numbers[list_numbers.length-1] == '=') {
-                list_numbers.pop('=')
-                expression = list_numbers.join("")
+            if (list_numbers[list_numbers.length-1] == '=') {  //create the expression from list_numbers after press "="
+                list_numbers.pop('=') //As the "=" was added to list_numbers, it'll be removed
+                expression = eval(list_numbers.join("")) //create the expression
                 console.log(expression)
 
                 try {
                     output.style.color = "black"
-                    output.innerHTML = eval(expression)
+                    if (expression % 1 != 0) {  //check if the number is decimal, if true, this will show like this: 3.1416 -> 3.14  
+                        output.innerHTML = expression.toFixed(2)
+                    } else { //if false, this will just return the number
+                        output.innerHTML = expression
+                    }
+
                 } catch {
                     output.style.color = "red"
                     output.style.fontSize = "30px"
-                    output.innerHTML = "<strong>[ERRO]</strong> Tente refazer o cálculo"
+                    output.innerHTML = "<strong>[ERROR]</strong> Try remake the calc, something went wrong!"
                 }
             }
         })

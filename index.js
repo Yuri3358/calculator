57bd18474d3f98e4029c2b814f37acea09ef580b
yuri3358 <yurialmeida917@gmail.com>
2022-08-20 15:31:37 -0300

    Console logs removed; clean and deleted added

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ function start() {
     for (const number of numbers) {
         number.addEventListener('click', (element) => {
 
-            list_numbers.push(element.target.id)
-            console.log(list_numbers)
+            list_numbers.push(element.target.id) //add the id to array "list_numbers"
+            let last_button = list_numbers[list_numbers.length-1] //last element from array
 
+<<<<<<< HEAD
             if (list_numbers[list_numbers.length-1] == '=') {  //create the expression from list_numbers after press "="
                 list_numbers.pop('=') //As the "=" was added to list_numbers, it'll be removed
                 expression = eval(list_numbers.join("")) //create the expression
@@ -18,18 +19,48 @@ function start() {
 
                 try {
                     output.style.color = "black"
+=======
+            try {
+                output.style.color = "black"
+                output.style.fontSize = "40px"
+                switch (last_button) {
+                    case "=":
+                        list_numbers.pop('=') 
+                        expression = eval(list_numbers.join("")) 
+                        break
+    
+                    case "clear":
+                        list_numbers.length = 0
+                        output.innerText = "aaa"
+                        break
+    
+                    case "delete":
+                        list_numbers.pop('delete')
+                        list_numbers.pop(last_button)
+                        break
+                }
+>>>>>>> 0ba7176 (Console logs removed; clean and deleted added)
                     if (expression % 1 != 0) {  //check if the number is decimal, if true, this will show like this: 3.1416 -> 3.14  
                         output.innerHTML = expression.toFixed(2)
                     } else { //if false, this will just return the number
                         output.innerHTML = expression
                     }
+<<<<<<< HEAD
 
                 } catch {
                     output.style.color = "red"
                     output.style.fontSize = "30px"
                     output.innerHTML = "<strong>[ERROR]</strong> Try remake the calc, something went wrong!"
+=======
+                
+                } catch {
+                    output.style.color = "red"
+                    output.style.fontSize = "20px"
+                    output.innerHTML = "<strong>[ERROR]</strong> Try remake the calc, something went wrong!"
+                    list_numbers.length = 0
+
+>>>>>>> 0ba7176 (Console logs removed; clean and deleted added)
                 }
-            }
         })
     }
 }
